243685744fafe0b66aa2593cc5a0a2dad726786a
Patricia Chun <yejeechun@gmail.com>
2017-06-09 17:28:34 -0400

    Form in view.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -4,24 +4,26 @@ from forms import SignUpForm
 # Create your views here.
 def home(request):
     title = "Welcome"
-    # if request.user.is_authenticated():
-    #     title = "My Title %s" %(request.user)
-
-
-    # add a form
-    # if request.method == "POST":
-    #     print request.POST
     form = SignUpForm(request.POST or None)
-
-    if form.is_valid():
-        instance = form.save(commit=False)
-        if not instance.full_name:
-            instance.full_name = "Justin"
-        instance.save()
-        print instance.email
-        print instance.timestamp
     context = {
         "title" : title,
         "form" : form
     }
+
+    if form.is_valid():
+        #form.save()
+        # print request.POST['email'] # not recommended
+        instance = form.save(commit=False)
+
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        # if not instance.full_name:
+        #     instance.full_name = "Justin"
+        instance.save()
+        context = {
+            "title" : "Thank you"
+        }
+
     return render(request, "home.html", context)
